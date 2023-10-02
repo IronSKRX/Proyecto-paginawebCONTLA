95ca6ca161cb41c10e6b7c3404b5b72255e0ae63
IronSKRX <cscontla2001@gmail.com>
2023-10-01 21:49:48 -0600

    first commit

---
+++ Conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$User = "root";
+$pass = "";
+
+$db ="inisiosesion";
+
+$conexion = mysqli_connect($host, $User, $Pass, $db);
+
+if 
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="cs/login.css">
+    <link rel="stylesheet" href="cs/cabecera.css">
+
+</head>
+<body>
+    <div class="Caja">
+   <form action="validar.php" method="post">
+   <h1 class="animate__animated animate__backInLeft">MANTENIMIENTO MOTOR SPORT</h1>
+   <p class="blanco">Usuario <input type="text" placeholder="Ingresa tu nombre" name="usuario"></p>
+   <p class="blanco">Contraseña <input type="password" placeholder="Ingresa tu Passwordnm" name="password"></p>
+   <input class="btn btn-success" type="submit" value="Ingresar">
+   
+   </form>
+</div> 
+
+
+</body>
+</html>
